c8a83b410c9a194910f52c2da472a257ae22ecb1
marymiranda1113 <73919921+marymiranda1113@users.noreply.github.com>
2023-07-13 18:42:22 -0600

    Deportista final

---
+++ ProyectoP2/src/proyectop2/Deportista.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectop2;
+
+/**
+ *
+ * @author 
+ */
+public class Deportista extends Persona {
+    public Deportista(String nombre, String apellido, int edad) {
+        super(nombre, apellido, edad);
+    }
+}
+
